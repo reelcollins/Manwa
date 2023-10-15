88f0312eb63afce91f8d972ff559cc1427492b37
reelcollins <elijahcollins254@gmail.com>
2023-10-15 12:12:45 +0300

    csrf

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,3 +135,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = ['https://django-server-production-a76c.up.railway.app']
