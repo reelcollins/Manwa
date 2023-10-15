0136686ec4491fa494cd6b0fbb8d6f99c89dff39
reelcollins <elijahcollins254@gmail.com>
2023-10-15 03:02:29 +0300

    home

--- frontend/views.py
+++ frontend/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.template import loader
 
 def frontend(request):
-  template = loader.get_template('kepha.html')
+  template = loader.get_template('home.html')
   return HttpResponse(template.render())
