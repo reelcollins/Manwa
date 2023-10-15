7dbb45df980f119b489537575b391a1df36df58e
reelcollins <elijahcollins254@gmail.com>
2023-10-15 04:26:45 +0300

    a lot of templates

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include # new
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
-
 urlpatterns = [
-    path('frontend/', views.frontend, name='frontend'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
+    # path('dashboard/', views.dashboard_view, name='dashboard'),
+    path("accounts/", include("django.contrib.auth.urls")),  # new
+   
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,28 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.utils import timezone
+from backend.models import Contact, Summary, Skill, Service, Fact, Project, Testimony, Education, Experience
 
-def frontend(request):
-  template = loader.get_template('home.html')
-  return HttpResponse(template.render())
+class HomeView(ListView):
+    model = Testimony
+    template_name = 'home.html'
+    context_object_name = 'testimonies'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['educations'] = Education.objects.all()
+        context['experiences'] = Experience.objects.all()
+
+        summary = Summary.objects.get()
+        for exp in context['experiences']:
+            exp.items = exp.description.split(',')
+
+        context['projects'] = Project.objects.all()
+        context['contacts'] = Contact.objects.get()
+        context['facts'] = Fact.objects.all()
+        context['skills'] = Skill.objects.all()
+        context['services'] = Service.objects.all()
+        context['dat'] = timezone.now().date()
+        context['summar'] = summary
+
+        return context
